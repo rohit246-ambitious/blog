0b9f146be19a23614a878b8205b18c7ac1271c47
Rohit Mhatre <rohitmhatre578@gmail.com>
2025-12-08 01:39:15 +0530

    Project setup complete: created blog app, defined Category model, and applied migrations

---
+++ blogs/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.category_name
