67e519697f1332e448055898c4e3ee1b63cc674f
Rohit Mhatre <rohitmhatre578@gmail.com>
2025-12-11 00:39:00 +0530

    Implemented category-wise blog listing page and added template inheritance to avoid code repetition

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Blog, Category
+
+def posts_by_category(request, category_id):
+    #fetch the posts that belong to the given category_id
+    posts = Blog.objects.filter(category_id=category_id, status="Published")
+
+    #undifined category handling
+    try:
+       category = Category.objects.get(pk=category_id)
+    except:
+       return redirect('home')
+    
+    #this is an alternative way to handle undifined category using get_object_or_404
+    #category = get_object_or_404(Category, pk=category_id)
+    context = {
+        'posts': posts,
+        'category': category
+    }
+    return render(request, 'posts_by_category.html', context)
