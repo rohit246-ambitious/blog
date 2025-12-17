71852a0fe7b803841d4e6da2250ac4f5dfd55d2b
Rohit Mhatre <rohitmhatre578@gmail.com>
2025-12-18 00:35:44 +0530

    Completed authentication flow with login and logout

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -31,4 +31,6 @@ urlpatterns = [
     #serch blog posts
     path('blogs/search/', BlogsView.search, name='search'),
     path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog_main/views.py
+++ blog_main/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import redirect, render
 from about.models import About
 from blogs.models import Blog, Category
 from .forms import resgisterForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 def home(request):
     featured_posts = Blog.objects.filter(is_featured=True, status="Published").order_by('updated_at')
@@ -32,3 +34,24 @@ def register(request):
         form = resgisterForm()
     context = {'form': form}
     return render(request, 'register.html', context)
+
+def login(request):
+    if request.method =='POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            #authenticate the user
+
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+            #log the user in
+            return redirect('home')
+    form = AuthenticationForm()
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
