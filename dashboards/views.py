06745471d1a56304d008560e58ae46b573ca1db4
Rohit Mhatre <rohitmhatre578@gmail.com>
2025-12-19 23:32:17 +0530

    Start dashboard design for editor and manager roles

---
+++ dashboards/views.py
@@ -0,0 +1,11 @@
+from blogs.models import Category, Blog
+from django.shortcuts import render
+
+def dashboard(request):
+    category_count = Category.objects.all().count()
+    blogs_count = Blog.objects.all().count()
+    context = {
+        'category_count': category_count,
+        'blogs_count': blogs_count,
+    }
+    return render(request, 'dashboard/dashboard.html', context)
