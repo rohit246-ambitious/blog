5abc90fd4fbc5a17d3688c988b9156632acb9ce2
Rohit Mhatre <rohitmhatre578@gmail.com>
2025-12-20 01:29:02 +0530

    Complete category CRUD functionality in the dashboard

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,6 +1,11 @@
 from blogs.models import Category, Blog
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required
 
+from .forms import CategoryForm
+from django.shortcuts import redirect
+
+@login_required(login_url='login')
 def dashboard(request):
     category_count = Category.objects.all().count()
     blogs_count = Blog.objects.all().count()
@@ -9,3 +14,37 @@ def dashboard(request):
         'blogs_count': blogs_count,
     }
     return render(request, 'dashboard/dashboard.html', context)
+
+@login_required(login_url='login')
+def categories(request):
+    return render(request, 'dashboard/categories.html')
+
+@login_required(login_url='login')
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+   
+    form = CategoryForm()
+    context = {'form': form}
+    return render(request, 'dashboard/add_category.html', context)
+
+@login_required(login_url='login')
+def edit_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+    form = CategoryForm(instance=category)
+    context = {'form': form, 'category': category, 'pk': pk}
+    return render(request, 'dashboard/edit_category.html', context)
+
+@login_required(login_url='login')
+def delete_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    category.delete()
+    return redirect('categories')
